9142ed08b40b573650c88563ae5b3e0c78b52b8a
cyoungLuma <106674708+cyoungLuma@users.noreply.github.com>
2022-08-12 15:20:59 -0400

    Add new sites

--- new_product_identifier/new_product_identifier.py
+++ new_product_identifier/new_product_identifier.py
@@ -67,25 +67,63 @@ class Driver:
         ]
 
         return bmo_active_products
+    
+    def get_nbcss_products(self):
+        # Setup for nbc_ss
+        nbcss_act_dict = {}
+        # Get first page
+        url = 'https://www.nbcstructuredsolutions.ca/listeProduits.aspx?mode=previous'
+        page = self.driver.get(url)
+        time.sleep(2)
+        nbcss_act_dict[0] = pd.read_html(self.driver.page_source)[0]
+        nbcss_act_dict[0]['urls'] = [
+            self.driver.find_element_by_xpath('//*[@id="ctl00_cphMain_lvProducts_ctrl{}_lnkProduit"]'.format(i)).get_attribute('href') for i in range(len(nbcss_act_dict[0]))
+        ]
+        # Get remaining 4 pages
+        for num in range(2, 6):
+            self.driver.find_element_by_xpath('//*[@id="ctl00_cphMain_dpProductsHaut"]/a[{}]'.format(str(num))).click()
+            time.sleep(2)
+            nbcss_act_dict[num] = pd.read_html(self.driver.page_source)[0]
+            nbcss_act_dict[num]['urls'] = [
+                self.driver.find_element_by_xpath('//*[@id="ctl00_cphMain_lvProducts_ctrl{}_lnkProduit"]'.format(i)).get_attribute('href') for i in range(len(nbcss_act_dict[num]))
+            ]
+        # Combine the dataframes
+        nbcss_active_products = pd.concat([nbcss_act_dict[k] for k in nbcss_act_dict.keys()], ignore_index=True)
+        # Create pdw cusip for comparison
+        nbcss_active_products['pdwCusip'] = [
+            'CA' + i if len(i) == 7 else 'C' + i for i in nbcss_active_products['FundSERV']
+        ]
+
+        return nbcss_active_products
 
 
-    def compare_bmo_pdw(self, bmo_prods, pdw_prods):
+    def compare_site_to_pdw(self, site, site_prods, pdw_prods):
         ''''''
         # Filter out any products already present in pdw
-        new_bmo_active_products = bmo_prods[
-            (bmo_prods['JHN Code / Cusip'].isin(pdw_prods['cusip'])==False) &
-            (bmo_prods['JHN Code / Cusip'].isin(pdw_prods['isin'])==False)
+        new_active_products = site_prods[
+            (site_prods['pdwCusip'].isin(pdw_prods['cusip'])==False) &
+            (site_prods['pdwCusip'].isin(pdw_prods['isin'])==False)
         ]
         # Create list of product urls to return
-        urls = ['https://www.bmonotes.com/Note/' + i for i in new_bmo_active_products['JHN Code / Cusip']]
+        if site == 'bmo':
+            urls = ['https://www.bmonotes.com/Note/' + i for i in new_active_products['JHN Code / Cusip']]
+        elif site == 'nbcss':
+            urls = new_active_products['urls'].unique()
 
         return urls
 
 if __name__ == '__main__':
     # Instantiate Driver
     driver = Driver()
+    
     # Get cusip and isin for all products added to pdw in the past week
     recent_pdw_products_dict = driver.get_recent_pdw_products()
+    
     # Get new BMO products
     bmo_prods = driver.get_bmo_products()
-    driver.compare_bmo_pdw(bmo_prods, recent_pdw_products_dict)
+    print(driver.compare_site_to_pdw('bmo', bmo_prods, recent_pdw_products_dict))
+    
+    # Get new NBCSS products
+    nbcss_prods = driver.get_nbcss_products()
+    print(driver.compare_site_to_pdw('nbcss', nbcss_prods, recent_pdw_products_dict))
+    
