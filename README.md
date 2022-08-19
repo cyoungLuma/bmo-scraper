d7390ec352871a954f44f66853ec203313d4f421
YvonneShuiLuma <yshui@lumafintech.com>
2022-08-19 13:28:51 -0400

    get token and post to PDW with lambda

--- README.md
+++ README.md
@@ -13,5 +13,10 @@ $ pip install -r requirements.txt
 ### Usage
 In the poc folder, either run the BmoScraper.py script as an executable, or interactively run BmoScraper.ipynb to view the results step by step.
 
+In the lambdas_write_to_pdw/lambda_function.py file, changde client id and client secret. 
+
+In the lambdas_write_to_pdw/product.json file, changde the product isin so you won't get duplicates error. 
+
+
 ### To Do
 BMO Examples.xlsx is included in this repo.  The performance of the BmoScraper object could be improved by just iterating once per note.  Further testing with additional notes is needed, and exception handling may need to be built in as development continues.
---
+++ lambdas_write_to_pdw/oauth_access_token.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import sys
+
+
+def get_new_token(client_id, client_secret):
+    """get new Oauth token, currently using PI's secrets"""
+    auth_server_url = 'https://bdev.lumafintech.com/api/auth-service/oauth/token'
+
+    token_req_payload = {'grant_type': 'client_credentials', 'scope': 'serverclient'}
+
+
+    token_response = requests.post(auth_server_url,
+                                   data=token_req_payload, verify=False, allow_redirects=False,
+                                   auth=(client_id, client_secret))
+
+    if token_response.status_code != 200:
+        print("Failed to obtain token from the OAuth 2.0 server", file=sys.stderr)
+        sys.exit(1)
+
+    print("Successfully obtained a new token")
+    tokens = json.loads(token_response.text)
+    print(tokens['access_token'])
+    return tokens['access_token']
+
